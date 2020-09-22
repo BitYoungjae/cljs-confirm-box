ed2704144e8f8cc25150f08e8e9ab677d596140d
BitYoungjae <bityoungjae@gmail.com>
2020-09-22 10:31:11 +0900

    init

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # CLJS 연습용 템플릿
 
-개인 사용 목적으로 구성됨
+cljs + reagent + re-frame 연습용 starter
+
+## 의존성
+
+### npm
+
+- react 16.13.0
+- react-dom 16.13.0
+
+### cljs
+- reagent 1.0.0-alpha2
+- re-frame 1.1.1
+- re-frisk 1.3.4
+- binaryage/oops 0.7.0
 
 ## 사용법
 
 ```sh
-npm i
-npm run start
+npm start
 ```
--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,8 +1,18 @@
-(ns app.core)
+(ns app.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]
+            [re-frame.core :as rf]
+            [oops.core :refer [ocall]]
+            [cljs.core.async :as ca :refer [go]]
+            [cljs.core.async.interop :refer-macros [<p!]]))
+
+(defn app []
+  [:div "Hello world!"])
 
 (defn ^:dev/after-load start [] ; HMR 을 위한 코드 ^:dev~는 메타 태그이다
   ; 매번 파일 저장시마다 이 함수가 호출이 된다.
-  (js/console.log "안녕 CLJS"))
+  (let [app-container (ocall js/document "querySelector" "#app")]
+    (rdom/render app app-container)))
 
 (defn ^:export init [] ; entry point. devserver 실행시 호출된다.
   (start))
